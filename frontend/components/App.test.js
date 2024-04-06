5d89065e90c100cf168b79f2fa56ce575f143b79
Natalie Thompson <nataliethompson2314@gmail.com>
2024-04-06 12:55:52 -0700

    sdfsf

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -42,9 +42,7 @@ describe("App Functional Component", () => {
 
     user = userEvent.setup();
 
-    header = screen.getByText((content, element) => {
-      return element.tagName.toLowerCase() === 'h1' && /Welcome to the GRID/i.test(content);
-    });
+    header = screen.getByRole("heading", { level: 1 });
     coordinatesHeading = screen.getByText(/Coordinates/);
     movesHeading = screen.getByText(/You moved/);
     buttonLeft = screen.getByTestId("LEFT");
